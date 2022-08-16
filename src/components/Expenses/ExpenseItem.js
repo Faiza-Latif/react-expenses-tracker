c54a19dacf756a6660b54f83f6378a02accf9fe3
Faiza-Latif <faiza@outlook.pt>
2022-08-16 17:30:46 +0100

    useState hook and understanding state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,29 @@
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import {useState} from "react";
 
 function ExpenseItem(props) {
     //props are key value pairs
+    const [title, setTitle] = useState(props.title);
+    //useState returns an array where the first element is the current state value and
+    // the second element is the function to change it
 
+    const updateTitle = () => {
+        //state updating function ->
+        // the component where this method is called will be executed again
+        setTitle('hello')
+    }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
+                <button onClick={updateTitle}>Change title</button>
             </div>
+
         </Card>
     );
 }
